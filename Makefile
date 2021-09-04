7e210ef61cffcbb1ced0216efcf9d763c0f156e0
spin6lock <spin6lock@users.noreply.github.com>
2021-09-04 11:30:38 +0800

    change dir, add sdist for pypi

---
+++ Makefile
@@ -0,0 +1,22 @@
+all: pysproto test
+.PHONY : all
+
+pysproto: src/pysproto/python_sproto.c
+	python3 setup.py build --build-lib build
+
+test: pysproto
+	cp build/*.so test/
+	cd test && python3 test.py && python3 test_wild_pointer.py && python3 test_mem.py
+
+clean:
+	rm -f *.so
+	rm -rf python_sproto.egg-info
+	rm -rf build
+	rm -rf dist
+	rm -f core
+
+sdist:
+	python3 setup.py sdist
+
+publish:
+	twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
+import setuptools
 from distutils.core import setup, Extension
 
 pysproto_module = Extension('pysproto',
-        sources = ["python_sproto.c", "sproto.c"])
+        sources = ["src/pysproto/python_sproto.c", "src/pysproto/sproto.c"])
 
-setup (name = "python_sproto",
+with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+setup(name = "pysproto",
         version = '0.1',
         description = "python binding to sproto, a serialize library",
         long_description = "Documentation and bug report: http://github.com/spin6lock/python-sproto",
         author = "spin6lock",
+        author_email = "UNKNOWN",
+        package_dir = {"": "src"},
+        packages = setuptools.find_packages(where="src"),
+        package_data = {"pysproto": ["src/pysproto/msvcint.h", "src/pysproto/sproto.h"]},
+        python_requires = ">=3.6",
         license = "MIT",
-        url="http://github.com/spin6lock/python-sproto",
-        keywords=["sproto", "serialization"],
+        url = "http://github.com/spin6lock/python-sproto",
+        keywords = ["sproto", "serialization"],
         ext_modules = [pysproto_module])
